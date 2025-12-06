3c6b88479aa04ee61ab7cf499b47e43e4e72d39c
muhammed shafir <muhammedshafirweb@gmail.com>
2025-12-06 21:18:07 +0530

    arranged into multiple file for Readability

---
+++ routes.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write(
+      "<html><head><title>Node Js Server</title></head><Body><h1>Hi from server</h1><form action='/message' method='POST'><input label='write message' type='text' name='message'><button type='submit'>submit</button></form></Body></html>"
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      fs.writeFile(
+        "message.txt",
+        Buffer.concat(body).toString().split("=")[1],
+        (err) => {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          return res.end();
+        }
+      );
+    });
+  }
+};
+
+module.exports = requestHandler;
